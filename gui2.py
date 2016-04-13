3e9cfd4817d273dea9ba9a5345f30bd4993debf7
Kai Munechika <kaim808@gmail.com>
2016-04-13 18:00:58 -0400

    Added files via upload

---
+++ gui2.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+master = tk.Tk()
+
+Audiofiles = ["A.wav"] #I'll add more sounds to this. 
+buttonlst = ['A','B','C','D','E','F','G']
+
+
+class Pianokey(tk.Button):
+    def __init__(self, text, *args):
+        self.text = text
+        tk.Button.__init__(self, height= 12, width=4, text = self.text, command=self.callback,*args)
+        
+    def callback(self):
+        for n,i in enumerate(buttonlst):
+            if i == self.text:
+                print(i)
+                #play Audiofiles[n] 
+                
+C = Pianokey(text='C')
+C.grid(row=0, column=0)
+
+D = Pianokey(text='D')
+D.grid(row=0, column=1)
+
+E = Pianokey(text='E')
+E.grid(row=0, column=2)
+
+F = Pianokey(text='F')
+F.grid(row=0, column=3)
+
+G = Pianokey(text='G')
+G.grid(row=0, column=4)
+
+A = Pianokey(text='A')
+A.grid(row=0, column=5)
+
+B = Pianokey(text='B')
+B.grid(row=0, column=6)
+
+master.mainloop()
