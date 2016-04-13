b12af14f6baa9babc721aab03f9c2b9119b6ced6
Kai Munechika <kaim808@gmail.com>
2016-04-13 19:34:18 -0400

    Black key addition
    black key class + black key objects + grid placement adjustments

--- gui2.py
+++ gui2.py
@@ -9,33 +9,56 @@ buttonlst = ['A','B','C','D','E','F','G']
 class Pianokey(tk.Button):
     def __init__(self, text, *args):
         self.text = text
-        tk.Button.__init__(self, master=master, height= 12, width=4, text = self.text, command=self.callback,*args)
+        tk.Button.__init__(self, master=master, height=36, width=12, text=self.text,\
+                           command=self.callback, bg="white", *args)
         
     def callback(self):
         for n,i in enumerate(buttonlst):
             if i == self.text:
                 print(i)
                 #play Audiofiles[n] 
-                
+
+class Blackkey(Pianokey):
+    def __init__(self, text, *args):
+        self.text = text
+        tk.Button.__init__(self, master=master, height=15, width=8, text=self.text,\
+                           command=self.callback, bg="black", *args)
+    
+
 C = Pianokey(text='C')
-C.grid(row=0, column=0)
+C.grid(row=1, column=0)
 
 D = Pianokey(text='D')
-D.grid(row=0, column=1)
+D.grid(row=1, column=1)
 
 E = Pianokey(text='E')
-E.grid(row=0, column=2)
+E.grid(row=1, column=2)
 
 F = Pianokey(text='F')
-F.grid(row=0, column=3)
+F.grid(row=1, column=3)
 
 G = Pianokey(text='G')
-G.grid(row=0, column=4)
+G.grid(row=1, column=4)
 
 A = Pianokey(text='A')
-A.grid(row=0, column=5)
+A.grid(row=1, column=5)
 
 B = Pianokey(text='B')
-B.grid(row=0, column=6)
+B.grid(row=1, column=6)
+
+Csharp = Blackkey(text='C#')
+Csharp.grid(row=0, column=0, rowspan=2, columnspan=2, sticky="N")
+
+Dsharp = Blackkey(text='D#')
+Dsharp.grid(row=0, column=1, rowspan=2, columnspan=2, sticky="N")
+
+Fsharp = Blackkey(text='F#')
+Fsharp.grid(row=0, column=3, rowspan=2, columnspan=2, sticky="N")
+
+Gsharp = Blackkey(text='G#')
+Gsharp.grid(row=0, column=4, rowspan=2, columnspan=2, sticky="N")
+
+Asharp = Blackkey(text='A#')
+Asharp.grid(row=0, column=5, rowspan=2, columnspan=2, sticky="N")
 
 master.mainloop()
