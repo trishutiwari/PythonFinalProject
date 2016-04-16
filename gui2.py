84b6a93ccc81699dcee9307dc2047a0c4f35edd7
trishutiwari <trtiwari@bu.edu>
2016-04-16 10:21:14 -0400

    fixed some bugs--still have problems with file reader

--- gui2.py
+++ gui2.py
@@ -1,6 +1,7 @@
 from tkinter import filedialog
 import tkinter as tk
 import winsound as sound
+from csv import reader
 
 
 master = tk.Tk()
@@ -8,39 +9,43 @@ master = tk.Tk()
 Audiofreq = [261,293,329,349,391,440,493,279,311,369,415,466] #just add the frequencies in the order that you put the names in buttonlst
 buttonlst = ['A','B','C','D','E','F','G','Csharp','Dsharp','Fsharp','Gsharp','Asharp']
 
-
 class Pianokey(tk.Button):
+    boo = False
     songlist = []
     def __init__(self, text):
         self.text = text
         tk.Button.__init__(self, master=master, height=36, width=12, text=self.text,\
                            command=self.callback, bg="white")
         
-    def callback(self,boo=False): #will need to eventually pass 'event' as an argument
+    def callback(self): #will need to eventually pass 'event' as an argument
         for n,i in enumerate(buttonlst):
             if i == self.text:
                 sound.Beep(Audiofreq[n],1000)                
-                if boo:
+                if Pianokey.boo:
                     Pianokey.songlist.append(i)
         
 def filewriter():
+    Pianokey.boo = False
     songlist = Pianokey.songlist
-    newfile = filedialog.asksaveasfilename()
-    newfile = open(newfile,'w')
-    newfile.write(str(songlist))
+    newfile = filedialog.asksaveasfile(mode='w',filetypes=(('text files','.txt'),))
+    if not newfile:
+        return None
+    newfile.write(str(songlist[1:-1]))
+    Pianokey.songlist = []
 
 def filereader():
-    songlist = Pianokey.songlist
     filename = filedialog.askopenfilename()
     newfile = open(filename,'r')
-    songlist = newfile.read()
-    return songlist
-
-def songplayer():
-    songlist = filereader()
+    songlist = reader(newfile, delimiter=',') #I don't know the representation of the datatype the reader returns
+    print(songlist)
     for i in songlist:
         n = buttonlst.index(i)
         sound.Beep(Audiofreq[n],1000)
+    newfile.close()
+
+
+def songrecorder():
+    Pianokey.boo = True
 
 class Blackkey(Pianokey):
     def __init__(self, text):
@@ -49,10 +54,11 @@ class Blackkey(Pianokey):
                            command=self.callback, bg="black", fg="white")
 
 class otherkeys(tk.Button):
-    def __init__(self,text):
+    def __init__(self,text,command):
+        self.command = command
         self.text = text
         tk.Button.__init__(self, master=master,text=self.text,width=5,\
-        font=("Arial",24,"bold"))
+        font=("Arial",24,"bold"),command=self.command)
 
 C = Pianokey(text='C')
 C.grid(row=1, column=0)
@@ -97,16 +103,13 @@ Gsharp.grid(row=1, column=4, columnspan=2, sticky="N")
 Asharp = Blackkey(text='Asharp')
 Asharp.grid(row=1, column=5, columnspan=2, sticky="N")
 
-record = otherkeys(text='record')
-record.command=Pianokey.callback(record,True)
+record = otherkeys(text='record', command=songrecorder)
 record.grid(row=2,columnspan=2)
 
-play = otherkeys(text='play')
-play.command=filereader
+play = otherkeys(text='play', command=filereader)
 play.grid(row=2,column=3,columnspan=2)
 
-stop = otherkeys(text='stop')
-stop.command=filewriter
+stop = otherkeys(text='stop', command=filewriter)
 stop.grid(row=2,column=5,columnspan=2)
 
 master.resizable(width=False, height=False)
