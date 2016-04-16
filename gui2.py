81294a3363e1ce55af68db12b27bbc83037a747e
Kai Munechika <kaim808@gmail.com>
2016-04-16 03:22:11 -0400

    Incomplete update
    Automated complete audiofreq and buttonlst lists. Now I’m working on a
    loop to make all the buttons. We don’t have to use every note I put up;
    we can just specify a range or number of keys for the piano once I
    figure out everything. Too tired to finish tonight but will work on it
    as soon as I wake up

--- gui2.py
+++ gui2.py
@@ -9,6 +9,36 @@ Audiofreq = [261,293,329,349,391,440,493,279,311,369,415,466] #just add the freq
 buttonlst = ['A','B','C','D','E','F','G','Csharp','Dsharp','Fsharp','Gsharp','Asharp']
 
 
+# Each row represents 1 octave - rounded each freq to the nearest whole number; source: http://www.seventhstring.com/resources/notefrequencies.html
+# 9 octaves total; pianos typically go from freq ~28 to 4186
+
+#            C       C#      D       D#      E       F       F#      G       G#      A       A#      B 
+#Audiofreq= [16,     17,     18,     19,     21,     22,     23,     24,     26,     28,     29,     31,\
+#            33,     35,     37,     39,     41,     44,     46,     49,     52,     55,     58,     62,\
+#            65,     69,     73,     78,     82,     87,     92,     98,     104,    110,    116,    124,\
+#            131,    139,    147,    156,    165,    175,    185,    196,    208,    220,    233,    247,\
+#            262,    277,    294,    311,    330,    349,    370,    392,    415,    440,    466,    494,\
+#            523,    554,    587,    622,    659,    698,    740,    784,    831,    880,    932,    988,\
+#            1047,   1109,   1175,   1245,   1319,   1397,   1480,   1568,   1661,   1760,   1865,   1976,\
+#            2093,   2217,   2349,   2489,   2637,   2794,   2960,   3136,   3322,   3520,   3729,   3951,\
+#            4186,   4435,   4699,   4978,   5274,   5588,   5920,   6272,   6645,   7040,   7459,   7902]
+
+#buttonlst=['C0', 'Csharp0', 'D0', 'Dsharp0', 'E0', 'F0', 'Fsharp0', 'G0', 'Gsharp0', 'A0', 'Asharp0', 'B0',\
+#           'C1', 'Csharp1', 'D1', 'Dsharp1', 'E1', 'F1', 'Fsharp1', 'G1', 'Gsharp1', 'A1', 'Asharp1', 'B1',\
+#           'C2', 'Csharp2', 'D2', 'Dsharp2', 'E2', 'F2', 'Fsharp2', 'G2', 'Gsharp2', 'A2', 'Asharp2', 'B2',\
+#           'C3', 'Csharp3', 'D3', 'Dsharp3', 'E3', 'F3', 'Fsharp3', 'G3', 'Gsharp3', 'A3', 'Asharp3', 'B3',\
+#           'C4', 'Csharp4', 'D4', 'Dsharp4', 'E4', 'F4', 'Fsharp4', 'G4', 'Gsharp4', 'A4', 'Asharp4', 'B4',\
+#           'C5', 'Csharp5', 'D5', 'Dsharp5', 'E5', 'F5', 'Fsharp5', 'G5', 'Gsharp5', 'A5', 'Asharp5', 'B5',\
+#           'C6', 'Csharp6', 'D6', 'Dsharp6', 'E6', 'F6', 'Fsharp6', 'G6', 'Gsharp6', 'A6', 'Asharp6', 'B6',\
+#           'C7', 'Csharp7', 'D7', 'Dsharp7', 'E7', 'F7', 'Fsharp7', 'G7', 'Gsharp7', 'A7', 'Asharp7', 'B7',\
+#           'C8', 'Csharp8', 'D8', 'Dsharp8', 'E8', 'F8', 'Fsharp8', 'G8', 'Gsharp8', 'A8', 'Asharp8', 'B8']
+
+
+#for i in range(len(Audiofreq)):
+#    globals()[(str(buttonlst[i])+'{}').format(octave) = Pianokey(text=buttonlst[i])
+
+
+
 class Pianokey(tk.Button):
     songlist = []
     def __init__(self, text):
